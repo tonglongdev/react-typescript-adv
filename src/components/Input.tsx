7af149e5901a0846db3e973f84dffc4e157f2826
TongLongDev <tonglong.dev@gmail.com>
2026-05-14 00:59:36 +0700

    forwardRef

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,15 +1,19 @@
-import type { ComponentPropsWithoutRef } from 'react';
+import { forwardRef, type ComponentPropsWithoutRef } from "react";
 
 type InputProps = {
   label: string;
   id: string;
-} & ComponentPropsWithoutRef<'input'>;
+} & ComponentPropsWithoutRef<"input">;
 
-export default function Input({label, id, ...props}: InputProps) {
-  return (
-    <p>
-      <label htmlFor={id}>{label}</label>
-      <input id={id} {...props} />
-    </p>
-  );
-}
+const Input = forwardRef<HTMLInputElement, InputProps>(
+  ({ label, id, ...props }, ref) => {
+    return (
+      <p>
+        <label htmlFor={id}>{label}</label>
+        <input id={id} {...props} ref={ref} />
+      </p>
+    );
+  },
+);
+
+export default Input;
